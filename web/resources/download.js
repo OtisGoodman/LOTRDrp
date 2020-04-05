d26cf68e04cc63806131f37becf0b8d9f18ab7ef
OtisGoodman <hubbert.pumpkin@gmail.com>
2020-04-05 10:35:11 -0500

    Website Update 1.8

--- web/resources/download.js
+++ web/resources/download.js
@@ -1,12 +1,10 @@
-function downloadURI(uri, name) 
-{
-    var link = document.createElement("a");
-    link.download = name;
-    link.href = uri;
-    link.click();
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  link.click();
 }
-function downloadLatest(){
-    downloadURI("../builds/LOTRDRP-1.7.jar","LOTRDRP-1.7.jar")
-    downloadURI("../builds/discord-rpc-1.3.1-all.jar","discord-rpc-1.3.1-all.jar")
-
+function downloadLatest() {
+  downloadURI("../builds/LOTRDRP-1.8.jar", "LOTRDRP-1.8.jar");
+  downloadURI("../builds/discord-core.jar","discord-core.jar");
 }
